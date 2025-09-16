2aef1809a2856f09042c401e5a84edcbc84f0317
Andres Felipe Patarroyo Muñoz <159494183+AndresPatarroyo1517@users.noreply.github.com>
2025-09-16 18:27:29 -0500

    template from rollaball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform particulas;
+    private ParticleSystem systemParticulas;
+    private Vector3 posicion;
+    public float speed;
+    private Rigidbody rb;
+    private int contador;
+    public TextMeshProUGUI textoContador;
+    private AudioSource audioRecoleccion;
+
+    void Start()
+    {
+        audioRecoleccion = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        systemParticulas = particulas.GetComponent<ParticleSystem>();
+        systemParticulas.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        // float moveJump = Input.GetAxis("Jump");
+        // float high = 0;
+        // if(moveJump) {
+        //     high = 5;
+        // }
+        // Debug.Log(moveJump);
+        Vector3 movimiento = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+        rb.AddForce(movimiento);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Recolectable"))
+        {
+            audioRecoleccion.Play();
+            contador += 1;
+            posicion = other.gameObject.transform.position;
+            particulas.position = posicion;
+            systemParticulas = particulas.GetComponent<ParticleSystem>();
+
+            systemParticulas.Play();
+            other.gameObject.SetActive(false);
+            textoContador.text = "Objetos recolectados: " + contador.ToString();
+            if (contador == 12)
+            {
+                Debug.Log(contador);
+                SceneManager.LoadScene(1);
+            }
+        }
+        else
+        {
+
+        }
+        
+    }
+}
