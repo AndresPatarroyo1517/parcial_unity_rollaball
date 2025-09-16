4b7b9821e25c8ffab880ceb0bc586641b1e9d193
Andres Felipe Patarroyo Muñoz <159494183+AndresPatarroyo1517@users.noreply.github.com>
2025-09-16 18:58:52 -0500

    parcial hecho

---
+++ Assets/Scripts/CuboRojo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboRojo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            transform.position += new Vector3(-1f, 0f, 0f);
+        }
+    }
+}
